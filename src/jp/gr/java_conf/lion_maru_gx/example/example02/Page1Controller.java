a0db5177fcbe7fb954402291dff55f89ba2674de
lion-maru-gx <lion.maru.gx@gmail.com>
2018-09-28 15:03:41 +0900

    work

--- src/jp/gr/java_conf/lion_maru_gx/example/example02/Page1Controller.java
+++ src/jp/gr/java_conf/lion_maru_gx/example/example02/Page1Controller.java
@@ -1,8 +1,18 @@
 package jp.gr.java_conf.lion_maru_gx.example.example02;
 
+
+import java.io.File;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class Page1Controller {
+    File            f                       = null;                     // 選択したファイルorディレクトリ情報
+    String          str                     = null;                     // 一時変数
+	@FXML
+	Label fileName;
     /**
      * ボタンクリックアクション
      */
@@ -18,4 +28,21 @@ public class Page1Controller {
         Main.getInstance().sendController(Main.Page.Page11);
     }
 
+    @FXML
+    protected void handleFileOpen() {
+        // ファイル選択ダイアログを表示し、選択したファイルパスを取得
+        FileChooser fc = new FileChooser();
+        fc.setTitle( "ファイル選択ダイアログ" );
+        fc.getExtensionFilters().addAll( new ExtensionFilter( "All Files", "*.*" ),
+                                         new ExtensionFilter( "Text Files", "*.txt" ),
+                                         new ExtensionFilter( "Image Files", "*.png", "*.jpg", "*.gif" ),
+                                         new ExtensionFilter( "Audio Files", "*.wav", "*.mp3", "*.aac" )
+                                        );
+        f   = fc.showOpenDialog(Main.getInstance().getStage());
+        str = ( f == null )? "選択なし" : f.toURI().toString() ;
+        fileName.setText("選択したファイル名 : " + str );
+
+    }
+    
+    
 }
